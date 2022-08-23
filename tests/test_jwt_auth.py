8c85268c88e3cd32b2ff9eb3b9f43a8935e17a1e
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2022-08-23 21:46:16 +0200

    updated tests

--- tests/test_jwt_auth.py
+++ tests/test_jwt_auth.py
@@ -132,3 +132,17 @@ async def test_path_exclusion() -> None:
 
         response = client.get("/west")
         assert response.status_code == HTTP_401_UNAUTHORIZED
+
+
+def test_security_schema() -> None:
+    jwt_auth = JWTAuth(token_secret="abc123", retrieve_user_handler=lambda _: None)
+    assert jwt_auth.security_schema.dict() == {
+        "bearerFormat": "JWT",
+        "description": "JWT api-key authentication and authorization.",
+        "flows": None,
+        "name": "Authorization",
+        "openIdConnectUrl": None,
+        "scheme": "Bearer",
+        "security_scheme_in": None,
+        "type": "http",
+    }
